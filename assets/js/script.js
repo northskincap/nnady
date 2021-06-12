548cfd2c69a4a504a9139c11857210a9f0a475dc
northskincap <85794876+northskincap@users.noreply.github.com>
2021-06-12 22:31:29 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.reviews').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        variableWidth: true,
+        touchThreshold: 5,
+    });
+});
